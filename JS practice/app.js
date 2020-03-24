f90bc5d7af68e80d7bd43254aaccfaf06e637296
mjoelbreen <mjoelbreen@gmail.com>
2020-03-24 17:10:21 -0500

    finished program

--- JS practice/app.js
+++ JS practice/app.js
@@ -1,15 +1,69 @@
-var cupCounter = document.getElementById("cup-counter");
-var coffeeButton = document.getElementById("coffee-button");
-var cupNum = 0;
-var errorMessage = document.getElementById("error-message");
+var hands = ['rock', 'paper', 'scissors'];
 
-coffeeButton.onclick = function () {
+const getHand = () => {
+    var hand = hands[Math.floor(Math.random()*3)];
+    return hand;
+}
+
+
+
+var player1 = {
+    name: 'Jeff',
+    hand: getHand()
+}
+
+var player2 = {
+    name: 'Rosa',
+    hand: getHand()
+}
+
+function playRound(player1, player2) {
     
-    if (cupNum < 3) {
-        cupNum += 1;
-        cupCounter.innerHTML = "Coffee cups: " + cupNum;
-    } else if (cupNum == 3) {
-        errorMessage.innerHTML = "I think I've had enough.";
+    
+    console.log("Jeff drew " + player1.hand);
+    console.log("Rosa drew " + player2.hand);
+
+    if (player1.hand == player2.hand) {
+        console.log("It's a tie.");
+    } else if (player1.hand == 'rock' && player2.hand == 'paper') {
+        console.log("Rosa wins!");
+        return player2;
+    } else if (player1.hand == 'rock' && player2.hand == 'scissors') {
+        console.log('Jeff wins!');
+        return player1;
+    } else if (player1.hand == 'paper' && player2.hand == 'scissors') {
+        console.log('Rosa wins!');
+        return player2;
+    } else if (player1.hand == 'paper' && player2.hand == 'rock') {
+        console.log('Jeff wins!');
+        return player1;
+    } else if (player1.hand == 'scissors' && player2.hand == 'rock') {
+        console.log("Rosa wins!");
+        return player2;
+    } else if (player1.hand == 'scissors' && player2.hand == 'paper') {
+        console.log("Jeff wins!");
+        return player1;
     }
 }
 
+function playGame(player1, player2, playUntil) {
+    var playUntil = 5;
+    var playerOneWins = 0;
+    var playerTwoWins = 0;
+    while (playerOneWins <= playUntil || playerTwoWins <= playUntil) {
+        if (playRound() == player1) {
+            playerOneWins += 1;
+        } else if (playRound() == player2) {
+            playerTwoWins += 1;
+        }
+        playRound();
+    };
+    if (playerOneWins > playerTwoWins) {
+        return player1;
+    } else if (playerOneWins < playerTwoWins) {
+        return player2;
+    }
+
+
+}
+playGame();
